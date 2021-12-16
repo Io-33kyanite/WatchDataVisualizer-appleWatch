525b0942b452558abd4547cb2be77d1bff5ffa73
Sasaki, I <57740535+33ky-sasaki@users.noreply.github.com>
2021-12-16 13:04:26 +0900

    Initial Commit

---
+++ WatchDataVisualizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchDataVisualizer
+//
+//  Created by SASAKI Iori on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
