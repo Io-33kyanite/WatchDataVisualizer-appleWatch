f07415487ce62f667b1ec6775adec65ae5d34a93
Sasaki, I <57740535+33ky-sasaki@users.noreply.github.com>
2021-12-16 19:17:54 +0900

    Initial Commit: could send accel values to iPhone from Apple Watch with Watch Connectivity

--- WatchDataVisualizer WatchKit Extension/InterfaceController.swift
+++ WatchDataVisualizer WatchKit Extension/InterfaceController.swift
@@ -7,20 +7,87 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
+import CoreMotion
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    let motionManager = CMMotionManager()
+    @IBOutlet weak var activationStatusLabel: WKInterfaceLabel!
+    @IBOutlet weak var reachableStatusLabel: WKInterfaceLabel!
+    @IBOutlet weak var accelLabel: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+        setupPhoneConnecting()
+        
+        startMotionSensing()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
 
+    @IBAction func sendMessage() {
+//        if WCSession.default.isReachable {
+//            let msg = "Hello, iPhone!"
+//            let data = ["message":msg]
+//
+//            WCSession.default.sendMessage(data, replyHandler: nil) { error in
+//                print(error)
+//            }
+//        }
+    }
+}
+
+
+//MARK: - Phone Connector
+extension InterfaceController: WCSessionDelegate {
+    private func setupPhoneConnecting() {
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("activationDidCompleteWith state= \(activationState.rawValue)")
+        if activationState.rawValue==2 {
+            self.activationStatusLabel.setText("status: active")
+        } else {
+            self.activationStatusLabel.setText("status: not active")
+        }
+    }
+}
+
+
+//MARK: - Motion Sensing
+
+extension InterfaceController {
+    private func startMotionSensing() {
+        if self.motionManager.isDeviceMotionAvailable {
+            self.motionManager.deviceMotionUpdateInterval = 0.1
+            self.motionManager.startDeviceMotionUpdates(to: OperationQueue(), withHandler: { (motion:CMDeviceMotion?, error:Error?) in
+                let x = motion?.userAcceleration.x
+                let y = motion?.userAcceleration.y
+                let z = motion?.userAcceleration.z
+                let dic = ["accel_x":x, "accel_y":y, "accel_z":z]
+                self.accelLabel.setText("x=\(x), y=\(y), z=\(z)")
+                
+                if WCSession.default.isReachable {
+                    self.reachableStatusLabel.setText("true")
+                    WCSession.default.sendMessage(dic, replyHandler: nil) { error in
+                        print(error)
+                    }
+                } else {
+                    self.reachableStatusLabel.setText("false")
+                }
+            })
+        }
+    }
 }
--- WatchDataVisualizer/ViewController.swift
+++ WatchDataVisualizer/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import WatchConnectivity
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var accelLabel_x: UILabel!
+    @IBOutlet weak var accelLabel_y: UILabel!
+    @IBOutlet weak var accelLabel_z: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupWatchConnecting()
     }
 
 
 }
 
+
+//MARK: - Watch Connector
+
+extension ViewController: WCSessionDelegate {
+    private func setupWatchConnecting() {
+        if WCSession.isSupported() {
+            WCSession.default.delegate = self
+            WCSession.default.activate()
+        }
+    }
+    
+    /// Message would be sent from my paired watch
+    internal func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        let accelX = message["accel_x"] as! Double
+        let accelY = message["accel_y"] as! Double
+        let accelZ = message["accel_z"] as! Double
+        
+        DispatchQueue.main.async {
+            self.accelLabel_x.text = String(accelX)
+            self.accelLabel_y.text = String(accelY)
+            self.accelLabel_z.text = String(accelZ)
+        }
+        
+        
+    }
+    
+    internal func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("activationDidCompleteWith state= \(activationState.rawValue)")
+    }
+    
+    internal func sessionDidBecomeInactive(_ session: WCSession) {
+        print("sessionDidBecomeInactive")
+    }
+    
+    internal func sessionDidDeactivate(_ session: WCSession) {
+        print("sessionDidDeactivate")
+    }
+    
+}
