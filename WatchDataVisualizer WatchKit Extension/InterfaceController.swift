e704796f29609be1d77477994377f645bcb4eabf
SASAKI, Iori <iorisasaki1234@gmail.com>
2022-04-26 18:45:14 +0900

    fix: to keep the app running in the inactive mode by extended runtime session

--- WatchDataVisualizer WatchKit Extension/InterfaceController.swift
+++ WatchDataVisualizer WatchKit Extension/InterfaceController.swift
@@ -13,39 +13,71 @@ import CoreMotion
 
 class InterfaceController: WKInterfaceController {
     
-    let motionManager = CMMotionManager()
-    @IBOutlet weak var activationStatusLabel: WKInterfaceLabel!
+    //MARK: User Setting
+    let MOTION_DATA_UPDATE_INTERVAL_s: Double = 0.1
+    
+    //MARK: #variable
+    var motionManager: CMMotionManager?
+    var erSession: WKExtendedRuntimeSession?
+    var dataUpdatingCounter: Int = 0
+    
+    //MARK: #component
+    @IBOutlet weak var watchConnectivityStatusLabel: WKInterfaceLabel!
     @IBOutlet weak var reachableStatusLabel: WKInterfaceLabel!
-    @IBOutlet weak var accelLabel: WKInterfaceLabel!
+    @IBOutlet weak var erSessionStatusLabel: WKInterfaceLabel!
+    @IBOutlet weak var StartERSessionButton: WKInterfaceButton!
+    @IBOutlet weak var debugField: WKInterfaceLabel!
+    @IBOutlet weak var countLabel: WKInterfaceLabel!
     
     override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+        countLabel.setText("00:00")
+        setupPhoneConnecting()
     }
     
     override func willActivate() {
-        setupPhoneConnecting()
-        
-        startMotionSensing()
+        // This method is called when watch view controller is about to be visible to user
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
+    
+    @IBAction func startMonitoringOnPhone() {
+        startMotionSensing()
+        
+        erSession = WKExtendedRuntimeSession()
+        erSession?.delegate = self
+        erSession?.start()
+    }
+}
 
-    @IBAction func sendMessage() {
-//        if WCSession.default.isReachable {
-//            let msg = "Hello, iPhone!"
-//            let data = ["message":msg]
-//
-//            WCSession.default.sendMessage(data, replyHandler: nil) { error in
-//                print(error)
-//            }
-//        }
+
+extension InterfaceController: WKExtendedRuntimeSessionDelegate {
+    func extendedRuntimeSessionDidStart(_ extendedRuntimeSession: WKExtendedRuntimeSession) {
+        print("extendedRuntimeSessionDidStart")
+        erSessionStatusLabel.setText("did start ERSession")
+        erSessionStatusLabel.setTextColor(.green)
+    }
+    
+    func extendedRuntimeSessionWillExpire(_ extendedRuntimeSession: WKExtendedRuntimeSession) {
+        print("extendedRuntimeSessionWillExpire")
+        erSessionStatusLabel.setText("will expire ERSession")
+        erSessionStatusLabel.setTextColor(.red)
+        resetMotionSensing()
+    }
+    
+    func extendedRuntimeSession(_ extendedRuntimeSession: WKExtendedRuntimeSession, didInvalidateWith reason: WKExtendedRuntimeSessionInvalidationReason, error: Error?) {
+        print("didInvalidateWith reason=\(reason)")
+        erSessionStatusLabel.setText("did invalidate ERSession")
+        debugField.setText("\(reason)")
+        erSessionStatusLabel.setTextColor(.red)
+        resetMotionSensing()
     }
 }
 
 
 //MARK: - Phone Connector
+
 extension InterfaceController: WCSessionDelegate {
     private func setupPhoneConnecting() {
         if WCSession.isSupported() {
@@ -56,11 +88,19 @@ extension InterfaceController: WCSessionDelegate {
     }
     
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        print("activationDidCompleteWith state= \(activationState.rawValue)")
-        if activationState.rawValue==2 {
-            self.activationStatusLabel.setText("status: active")
-        } else {
-            self.activationStatusLabel.setText("status: not active")
+        switch activationState.rawValue {
+        case 0:
+            watchConnectivityStatusLabel.setText("WC: not activated")
+            watchConnectivityStatusLabel.setTextColor(.lightGray)
+        case 1:
+            watchConnectivityStatusLabel.setText("WC: inactive")
+            watchConnectivityStatusLabel.setTextColor(.lightGray)
+        case 2:
+            watchConnectivityStatusLabel.setText("WC: active")
+            watchConnectivityStatusLabel.setTextColor(.green)
+        default:
+            watchConnectivityStatusLabel.setText("WC: ---")
+            watchConnectivityStatusLabel.setTextColor(.lightGray)
         }
     }
 }
@@ -69,25 +109,47 @@ extension InterfaceController: WCSessionDelegate {
 //MARK: - Motion Sensing
 
 extension InterfaceController {
-    private func startMotionSensing() {
-        if self.motionManager.isDeviceMotionAvailable {
-            self.motionManager.deviceMotionUpdateInterval = 0.1
-            self.motionManager.startDeviceMotionUpdates(to: OperationQueue(), withHandler: { (motion:CMDeviceMotion?, error:Error?) in
-                let x = motion?.userAcceleration.x
-                let y = motion?.userAcceleration.y
-                let z = motion?.userAcceleration.z
-                let dic = ["accel_x":x, "accel_y":y, "accel_z":z]
-                self.accelLabel.setText("x=\(x), y=\(y), z=\(z)")
-                
-                if WCSession.default.isReachable {
-                    self.reachableStatusLabel.setText("true")
-                    WCSession.default.sendMessage(dic, replyHandler: nil) { error in
-                        print(error)
+    func startMotionSensing() {
+        motionManager = CMMotionManager()
+        if motionManager!.isDeviceMotionAvailable {
+            motionManager!.deviceMotionUpdateInterval = MOTION_DATA_UPDATE_INTERVAL_s
+            
+            motionManager!.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: { (motion:CMDeviceMotion?, error:Error?) in
+                if let x = motion?.userAcceleration.x,
+                   let y = motion?.userAcceleration.y,
+                   let z = motion?.userAcceleration.z {
+                    
+                    self.dataUpdatingCounter += 1
+                    let timeInterval: Int = Int(self.MOTION_DATA_UPDATE_INTERVAL_s * Double(self.dataUpdatingCounter))
+                    let time_m = timeInterval / 60 % 60
+                    let time_s = timeInterval % 60
+                    self.countLabel.setText(String(format: "%02d:%02d", time_m, time_s))
+                    
+                    if WCSession.default.isReachable {
+                        self.reachableStatusLabel.setText("WC: reachable")
+                        self.reachableStatusLabel.setTextColor(.green)
+                        let dic: [String:Double] = ["accel_x":x, "accel_y":y, "accel_z":z]
+                        WCSession.default.sendMessage(dic, replyHandler: nil) { error in
+                            self.debugField.setText("Could not send a message in WCSession: \(error)")
+                        }
+                    } else {
+                        self.reachableStatusLabel.setText("WC: not reachable")
+                        self.reachableStatusLabel.setTextColor(.lightGray)
+                        self.resetMotionSensing()
                     }
-                } else {
-                    self.reachableStatusLabel.setText("false")
+                    
                 }
             })
+            
         }
     }
+    
+    func resetMotionSensing() {
+        if let motionManager = motionManager {
+            motionManager.stopDeviceMotionUpdates()
+            countLabel.setText("--:--")
+            dataUpdatingCounter = 0
+        }
+    }
+    
 }
